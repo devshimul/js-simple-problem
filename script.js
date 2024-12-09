457d76fc1ee23b050dc04158233f5531981eaf16
devshimul <shimul.bisd@gmail.com>
2024-12-09 21:37:36 +0600

    add even

---
+++ script.js
@@ -0,0 +1,25 @@
+// const marks = [14, 56, 98, 48, 51, 25, 364, 14];
+// function arrMultiply(numbers) {
+//   for (const mark of numbers) {
+//     if (mark % 2 === 0) {
+//     }
+//   }
+// }
+// const mulResult = arrMultiply(marks);
+
+const numbers = [14, 58, 45, 87, 96, 78, 47, 12, 98, 75, 15, 78, 96, 36];
+function evenNums(nums) {
+  const newArr = [];
+  for (const num of nums) {
+    if (num % 2 === 0) {
+      newArr.push(num);
+    }
+  }
+  let sum = 0;
+  for (const even of newArr) {
+    sum += even;
+  }
+  return sum;
+}
+const newNums = evenNums(numbers);
+console.log(newNums);
